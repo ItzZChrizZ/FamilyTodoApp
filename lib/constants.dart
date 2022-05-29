a168d1a58be4714e1785232e27d1ed57be2c1bc2
Ugur Ozkan <ugurozkan35@gmail.com>
2022-05-29 23:50:54 +0300

    Clear the codes and fix all errors

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
 
 class Constants {
-  static TextStyle regulerHeader = const TextStyle(
-      fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold);
+  static TextStyle title = const TextStyle(
+      fontSize: 20,
+      color: Color.fromRGBO(54, 69, 79, 1),
+      fontWeight: FontWeight.bold);
 
-  static TextStyle regulerHeaderRed = const TextStyle(
-      fontSize: 20, color: Colors.red, fontWeight: FontWeight.bold);
+  static TextStyle titleRed = const TextStyle(
+      fontSize: 20,
+      color: Color.fromRGBO(220, 20, 60, 1),
+      fontWeight: FontWeight.bold);
 
   static TextStyle regulerText = const TextStyle(
     fontSize: 18,
-    color: Colors.black,
+    color: Color.fromRGBO(54, 69, 79, 1),
     height: 1.5,
   );
 
-  static TextStyle regulerTodo = const TextStyle(
+  static TextStyle subTitle = const TextStyle(
     fontSize: 16,
-    color: Colors.black,
+    color: Color.fromRGBO(54, 69, 79, 1),
     fontWeight: FontWeight.bold,
   );
 
-  static Color backgroundColor = const Color.fromRGBO(14, 25, 58, 1);
+  static Color backgroundColor = const Color.fromRGBO(245, 245, 245, 1);
+  static Color iconColors = const Color.fromRGBO(255, 250, 250, 1);
+  static Color navBarColor = const Color.fromRGBO(54, 69, 79, 1);
+  static Color appBarColor = const Color.fromRGBO(54, 69, 79, 1);
 }
